e77c7e4912bac69b9489443c6c47767b8901c28c
Chris Russell <cruse1977123@gmail.com>
2025-05-22 12:50:51 +0100

    Initial - Step 4

--- lab-integration/lab_integration/runner.py
+++ lab-integration/lab_integration/runner.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import requests
 import json
 import csv
 from netboxlabs.diode.sdk.ingester import Device, Interface, IPAddres
@@ -12,7 +13,27 @@ class LabIntegration(Backend):
 
     def setup(self) -> Metadata:
         return Metadata(name="lab_integration", app_name="lab_integration_app", app_version="1.0.0")
-    
+
+    def load_from_controller(self, controller_url: str, controller_token: str) -> list:
+        ''' function to read from the controller and return a list '''
+        pdu_list = []
+        names = []
+        headers = {'Authorization': f'Token {controller_token}' }
+        r = requests.get(controller_url, headers=headers)
+        ''' get the brief list '''
+        if r.status_code == 200:
+            for item in r.json():
+                names.append(item["name"])
+        ''' loop through the brief list to get the individual pdus '''
+        for name in names:
+            iurl = f'{controller_url}?name={name}'
+            r = requests.get(iurl, headers=headers)
+            if r.status_code == 200:     
+                pdu_list.append(r.json())
+
+        return pdu_list
+
+
     def load_from_csv(self, filename: str) -> list:
         ''' function to read a csv file and return it as a list '''
         pdu_list = []
@@ -76,7 +97,11 @@ class LabIntegration(Backend):
         if method == 'csv':
             filename = p.get('config', {}).get('csv_filename')
             pdu_list = self.load_from_csv(filename)
-
+        elif method == "api":
+            controller_url = p.get('config', {}).get('controller_url')
+            controller_token = p.get('config', {}).get('controller_token')
+            pdu_list = self.load_from_controller(controller_url, controller_token)
+    
         if pdu_list:
             entities=self.transform_to_diode(pdu_list)
         return entities
